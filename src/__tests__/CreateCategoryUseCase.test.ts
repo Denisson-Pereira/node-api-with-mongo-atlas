c05abbfda8ae4a5d3999c1ecde159adb1c4f5bdb
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-01-03 09:14:02 -0300

    test: :test_tube: Add tests

---
+++ src/__tests__/CreateCategoryUseCase.test.ts
@@ -0,0 +1,45 @@
+import { ICategory } from "../domain/model/Category";
+import { IRepositoryCategory } from "../domain/ports/IRepositoryCategory";
+import { CreateCategoryUseCase } from "../domain/useCases/categories/CreateCategoryUseCase";
+
+
+describe("CreateCategoryUseCase", () => {
+    let mockRepository: jest.Mocked<IRepositoryCategory>;
+    let createCategoryUseCase: CreateCategoryUseCase;
+
+    beforeEach(() => {
+        mockRepository = {
+            save: jest.fn(),
+            getAllCategories: jest.fn(),
+            getCategoryById: jest.fn(),
+            deleteById: jest.fn(),
+            update: jest.fn(),
+        };
+
+        createCategoryUseCase = new CreateCategoryUseCase(mockRepository);
+    });
+
+    it("deve salvar uma categoria com sucesso", async () => {
+        const inputCategory: ICategory = { name: "Categoria A", description: "description A", image: "image A" };
+        const savedCategory: ICategory = { name: "Categoria A", description: "description A", image: "image A"  };
+
+        mockRepository.save.mockResolvedValue(savedCategory);
+
+        const result = await createCategoryUseCase.execute(inputCategory);
+
+        expect(mockRepository.save).toHaveBeenCalledWith(inputCategory);
+        expect(mockRepository.save).toHaveBeenCalledTimes(1);
+
+        expect(result).toEqual(savedCategory);
+    });
+
+    it("deve lançar erro se o repositório falhar", async () => {
+        mockRepository.save.mockRejectedValue(new Error("Erro ao salvar categoria"));
+
+        await expect(createCategoryUseCase.execute({ name: "Categoria A", description: "description A", image: "image A" }))
+            .rejects
+            .toThrow("Erro ao salvar categoria");
+
+        expect(mockRepository.save).toHaveBeenCalledTimes(1);
+    });
+});
---
+++ src/__tests__/GetAllCategoiesUseCase.test.ts
@@ -0,0 +1,37 @@
+import { ICategory } from "../domain/model/Category";
+import { IRepositoryCategory } from "../domain/ports/IRepositoryCategory"
+import { GetAllCategoriesUseCase } from "../domain/useCases/categories/GetAllCategoriesUseCase";
+
+describe("GetAllCategoryUseCase", () => {
+    let mockRepository: jest.Mocked<IRepositoryCategory>;
+    let getAllCategoriesUseCase: GetAllCategoriesUseCase;
+
+    beforeEach(() => {
+        mockRepository = {
+            save: jest.fn(),
+            getAllCategories: jest.fn(),
+            getCategoryById: jest.fn(),
+            deleteById: jest.fn(),
+            update: jest.fn(),
+        };
+
+        getAllCategoriesUseCase = new GetAllCategoriesUseCase(mockRepository);
+    })
+
+    it("deve obter todas as categorias com sucesso", async () => {
+        const categoria: ICategory[] = [
+            {
+                name: "Nome da categoria A",
+                description: "Descrição da categoria A",
+                image: "Imagem da categoria A"
+            }
+        ]
+
+        mockRepository.getAllCategories.mockResolvedValue(categoria);
+
+        const result = await getAllCategoriesUseCase.execute();
+
+        expect(mockRepository.getAllCategories).toHaveBeenCalledTimes(1);
+        expect(result).toEqual(categoria);
+    })
+})
