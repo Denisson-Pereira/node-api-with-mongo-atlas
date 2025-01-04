f62616ff1bbfabf7276413ba2cb4dea4a5f9c9d0
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-01-04 09:24:44 -0300

    test: :white_check_mark: Add test

---
+++ src/__tests__/DeleteCategoryUseCase.test.ts
@@ -0,0 +1,36 @@
+import { InvalidIdError } from "../domain/exceptions/InvalidIdError";
+import { ICategory } from "../domain/model/Category";
+import { IRepositoryCategory } from "../domain/ports/IRepositoryCategory"
+import { DeleteCategoryByIdUseCase } from "../domain/useCases/categories/DeleteCategoryByIdUseCase";
+
+describe("DeleteCategoryUseCase", () => {
+    let mockRepository: jest.Mocked<IRepositoryCategory>;
+    let deleteCategoryUseCase: DeleteCategoryByIdUseCase;
+
+    beforeEach(() => {
+        mockRepository = {
+            save: jest.fn(),
+            getAllCategories: jest.fn(),
+            getCategoryById: jest.fn(),
+            deleteById: jest.fn(),
+            update: jest.fn(),            
+        };
+
+        deleteCategoryUseCase = new DeleteCategoryByIdUseCase(mockRepository);
+    })
+
+    it("deve deletar uma categoria", async () => {
+        const id: string = "60c72b2f5f1b2c001f1a2c3d";
+
+        const deleted: string = `Categoria com id ${id} deletada!`
+
+        mockRepository.deleteById.mockResolvedValue(deleted);
+
+        const result = await deleteCategoryUseCase.execute(id);
+
+        expect(mockRepository.deleteById).toHaveBeenCalledTimes(1);
+        expect(result).toEqual(deleted);
+
+    })
+    
+})
--- src/domain/exceptions/InvalidIdError.ts
+++ src/domain/exceptions/InvalidIdError.ts
@@ -1,10 +1,6 @@
 export class InvalidIdError extends Error {
-    public statusCode: number;
-    
-    constructor() {
-        super("Id not found!");
-        this.statusCode = 404;
-
-        Object.setPrototypeOf(this, new.target.prototype);
+    constructor(message: string = "Id not found!") {
+        super(message);
+        this.name = "InvalidIdError"; 
     }
 }
