80fa79c4950890d16840baca1bce449f14ab91b7
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-01-04 15:47:54 -0300

    build: :rocket: deploy vercel

--- src/domain/exceptions/InvalidIdError.ts
+++ src/domain/exceptions/InvalidIdError.ts
@@ -1,6 +1,9 @@
 export class InvalidIdError extends Error {
+    public statusCode: number;
+
     constructor(message: string = "Id not found!") {
         super(message);
         this.name = "InvalidIdError"; 
+        this.statusCode = 400;
     }
 }
